6e1c0ad86ac62197f7cef26b5b57d1c31c9a454f
Ashley Meza <ashley.meza-medina@csedge.org>
2024-07-22 13:53:49 -0700

    gg

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def madlib():
 
 @app.route('/complete')
 def complete():
-    return 'Goodbye, World!'
+    return madlib
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
